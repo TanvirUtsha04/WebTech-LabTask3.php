210828dd49b6a47365c2e0fac0e39f0c08a7e608
Tanvir Ahmed <89394804+TanvirUtsha04@users.noreply.github.com>
2022-06-06 14:49:46 +0700

    Update WebTech1.php

--- WebTech1.php
+++ WebTech1.php
@@ -9,72 +9,12 @@ $p = 2*($length*$width);
 echo "area of reactangle = ".$p;
 ?><br>
 
-#2
-
-<?php
-$vat =15;
-$printExVat = 27;
-$vatToDue = ($priceExVat/100)*$vat;
-$totalPriceInVat = $priceExVat +$vatToDue;
-echo "Total Price Include VAT : ";
-echo number_format($totalPriceInVat, 2);
-?><br>
 
 
-#3
 
 
-<?php
-$number = 20;
-if($number % 2 == 0){
-	echo "Even";
-}
-else{
-	echo "Odd";
-}
-?><br>
 
 
-#4
-
-<?php
-$a = 6;
-$b = 7;
-$c = 10;
-
-if(($a>$b) && ($a>$c))
-{
-	echo " a is the largest number";
-}
-elseif(($b>$a) && ($b>$c))
-{
-	echo " b is the largest number";
-}
-
-else
-{
-	echo " c is the largest number";
-}
-?><br>
-
-
-#5
-
-
-<?php
-echo "The Odd number between 10 to 100";
-
-for ($i=10; $i<=100; $i++)
-
-{
-   if($i%2==1)
-   {
-       echo $i." ";
-   }
-
-
-}
-?><br>   
 
 
 
