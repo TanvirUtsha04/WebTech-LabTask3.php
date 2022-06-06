61a80a23ff6ed9987a52b8b3906484ba5234fe80
Tanvir Ahmed <89394804+TanvirUtsha04@users.noreply.github.com>
2022-06-06 14:45:22 +0700

    Add files via upload

---
+++ WebTech4.php
@@ -0,0 +1,19 @@
+<?php
+$a = 6;
+$b = 7;
+$c = 10;
+
+if(($a>$b) && ($a>$c))
+{
+	echo " a is the largest number";
+}
+elseif(($b>$a) && ($b>$c))
+{
+	echo " b is the largest number";
+}
+
+else
+{
+	echo " c is the largest number";
+}
+?><br>
